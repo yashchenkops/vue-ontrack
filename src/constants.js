134208118bc2f7d5ccc6bcb324f6302c0a59caff
Pavel <yashchenkodev@gmail.com>
2025-11-26 23:43:38 +0200

    feat: generate timeline hours

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outli
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
+export const HOURS_IN_DAY = 24
 
 export const NAV_ITEMS = {
   [PAGE_TIMELINE]: ClockIcon,
