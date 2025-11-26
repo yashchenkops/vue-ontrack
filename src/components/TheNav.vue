367b77290150057128a5d30fa02d7c0078f6e947
Pavel <yashchenkodev@gmail.com>
2025-11-26 22:14:36 +0200

    feat: header links

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,27 +1,16 @@
 <script setup>
-import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 import NavItem from './NavItem.vue'
 
+defineProps(['currentPage'])
+
+const emit = defineEmits(['navigate'])
+
 const navItems = {
   [PAGE_TIMELINE]: ClockIcon,
   [PAGE_ACTIVITIES]: ListBulletIcon,
-  [PAGE_PROGRESS]: ChartBarIcon
-}
-
-const currentPage = ref(normalizePageHash())
-
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-
-  if (Object.keys(navItems).includes(hash)) {
-    return hash
-  }
-
-  window.location.hash = PAGE_TIMELINE
-
-  return PAGE_TIMELINE
+  [PAGE_PROGRESS]: ChartBarIcon,
 }
 </script>
 
@@ -32,8 +21,8 @@ function normalizePageHash() {
         v-for="(icon, page) in navItems"
         :key="page"
         :href="`#${page}`"
-        :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
-        @:click="currentPage = page"
+        :class="{ 'pointer-events-none bg-gray-200': page === currentPage }"
+        @:click="emit('navigate', page)"
       >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
